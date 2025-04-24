96b8a7e029c26c2e82210091bc3324b7ebf7d88b
Tarun Jogalekar <146951878+tarunjogalekar7@users.noreply.github.com>
2025-04-25 00:06:51 +0530

    Update DayNameUsingSwitchCase.cpp

--- DayNameUsingSwitchCase.cpp
+++ DayNameUsingSwitchCase.cpp
@@ -10,22 +10,22 @@ int main()
         switch(day)
             {
                 case 1: cout<<"Monday"<<endl;
-                break;
-	   			case 2: cout<<"Tuesday"<<endl;
-	  		    break;
+                	break;
+	   	case 2: cout<<"Tuesday"<<endl;
+	  		break;
                 case 3: cout<<"Wednesday"<<endl;
   	            break;
-	            case 4: cout<<"Thursday"<<endl;
+	        case 4: cout<<"Thursday"<<endl;
 	            break;
-	            case 5: cout<<"Friday"<<endl;
+	        case 5: cout<<"Friday"<<endl;
 	            break;
-	            case 6: cout<<"Saturday"<<endl;
-                break;
-	            case 7: cout<<"Sunday"<<endl;
-                break;
-	            default: cout<<"Invalid Day Number"<<endl;
-	        }
+	        case 6: cout<<"Saturday"<<endl;
+                	break;
+	        case 7: cout<<"Sunday"<<endl;
+                	break;
+	        default: cout<<"Invalid Day Number"<<endl;
+	}
 	        
-		return 0;
+	return 0;
 	
     }
